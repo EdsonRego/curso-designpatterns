4f59a2e00172946fa157d92272aa080245f1a721
EdsonRego <edsonxrego@gmail.com>
2025-09-28 16:50:20 -0300

    Before Strategy

---
+++ src/main/java/com/edsonrego/curso_desingpatterns/strategy/Funcionario.java
@@ -0,0 +1,42 @@
+package com.edsonrego.curso_desingpatterns.strategy;
+
+import java.math.BigDecimal;
+
+public class Funcionario {
+
+    private String nome;
+    private BigDecimal salario;
+    private TipoContratacaoEnum tipoContratacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public TipoContratacaoEnum getTipoContratacao() {
+        return tipoContratacao;
+    }
+
+    public void setTipoContratacao(TipoContratacaoEnum tipoContratacao) {
+        this.tipoContratacao = tipoContratacao;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Funcionário: %-10s | Tipo: %-7s | Salário: R$ %,.2f",
+                nome,
+                tipoContratacao,
+                salario);
+    }
+}
---
+++ src/main/java/com/edsonrego/curso_desingpatterns/strategy/TipoContratacaoEnum.java
@@ -0,0 +1,9 @@
+package com.edsonrego.curso_desingpatterns.strategy;
+
+import java.math.BigDecimal;
+
+public enum TipoContratacaoEnum {
+    CLT,
+    PJ,
+    ESTAGIO
+}
